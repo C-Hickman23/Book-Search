8022be73a6a9769c1f0dedc276d91cf69b75272e
Cardon Hickman <cardonhickman@gmail.com>
2023-12-04 02:31:14 -0700

    reverted to old version of QUERY_ME

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,13 +4,11 @@ const {signToken, AuthenticationError} = require ("../utils/auth");
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
-            if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
-
-                return userData;
-            }
-
-            throw AuthenticationError;
+            const foundUser = await User.findOne({
+                _id: context.user._id,
+            })
+            .populate("savedBooks");
+            return foundUser;
         },
     },
     Mutation: {
